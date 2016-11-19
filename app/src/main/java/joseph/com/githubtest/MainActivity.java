fdb3988a0b737004d51e5caafa39856e68c7ce77
Brian <brncherin@gmail.com>
2016-11-18 21:22:56 -0500

    button to make random view color

--- app/src/main/java/joseph/com/githubtest/MainActivity.java
+++ app/src/main/java/joseph/com/githubtest/MainActivity.java
@@ -1,13 +1,32 @@
 package joseph.com.githubtest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button colorButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        colorButton = (Button)findViewById(R.id.colorButton);
+        colorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random rand = new Random();
+                int color = Color.argb(255, rand.nextInt(256), rand.nextInt(256), rand.nextInt(256));
+                findViewById(R.id.activity_main).setBackgroundColor(color);
+            }
+        });
+
     }
 }
